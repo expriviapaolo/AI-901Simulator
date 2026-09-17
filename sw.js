5ef1754cd3ef16bd7801d17dc84de7372418e5ed
expriviapaolo <97687065+expriviapaolo@users.noreply.github.com>
2026-09-17 10:39:46 +0200

    add

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // aperte come file locale (file://), dove però l'offline funziona comunque perché il file è già
 // interamente sul dispositivo.
 
-const CACHE_NAME = 'ai901-quiz-v6';
+const CACHE_NAME = 'ai901-quiz-v7';
 const ASSETS = [
   './AI901-Quiz-Web.html',
   './exam-config.js',
